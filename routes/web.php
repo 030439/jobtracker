3112ea28a910acfce9d21f90e56662f37b93e0f2
https://github.com/030439 <praimcahnd763@gmail.com>
2023-11-21 03:37:40 +0500

    auth done

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,6 +19,14 @@ Route::get('/', function () {
 });
 Route::prefix('user')->middleware('client')->group(function(){
     Route::controller(clientController::class)->group(function(){
-        Route::get('/','index');
+        Route::get('/user','index');
     });
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
