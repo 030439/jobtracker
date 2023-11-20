19eecf1c5092177da427599b141f03ed2707711f
https://github.com/030439 <praimcahnd763@gmail.com>
2023-11-20 10:38:25 +0500

    data in client layout file

---
+++ resources/views/layout/client.blade.php
@@ -0,0 +1,10 @@
+@include('client.include.head')
+
+@include('client.include.nav-chat')
+@include('client.include.header')
+@include('client.include.sidebar')
+
+    @yield('content')
+
+@include('client.include.footer')
+
