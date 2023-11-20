7af5b92bda2dffba9cf35d049c82f2c82f529fb0
https://github.com/030439 <praimcahnd763@gmail.com>
2023-11-20 22:16:35 +0500

    client controoler created

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(clientController::class)->group(function(){
+    Route::get('/user','index');
+});
