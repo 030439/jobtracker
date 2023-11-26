a8a979eb937e13ce95f20737723101a900e3b98e
https://github.com/030439 <praimcahnd763@gmail.com>
2023-11-26 14:17:29 +0500

    payment controller methods set

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Stripe\Stripe;
+use Stripe\PaymentIntent;
 
 class PaymentController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('payment.index');
+    }
+
+    public function createPaymentIntent(Request $request)
+    {
+        Stripe::setApiKey(env('STRIPE_SECRET'));
+
+        $intent = PaymentIntent::create([
+            'amount' => $request->amount,
+            'currency' => 'usd',
+            // Other payment intent parameters
+        ]);
+
+        return response()->json(['client_secret' => $intent->client_secret]);
+    }
+
+    public function paymentSuccess()
+    {
+        return view('payment.success');
+    }
 }
